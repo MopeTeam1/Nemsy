1c5546dcc9ed13f5dfdf994514b6ae10889f5c17
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-11-07 21:08:16 +0900

    feat: 하단바 액티비티에 발의법률안 목록 프래그먼트 추가

---
+++ app/src/main/java/com/example/nemsy/BottomNavActivity.java
@@ -0,0 +1,14 @@
+package com.example.nemsy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
