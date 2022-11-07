794b08a454f6bbfb57237ea9b5cf9820711ab3df
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-11-07 21:11:48 +0900

    feat: 발의법률안 목록 대수 스피너

--- app/src/main/java/com/example/nemsy/BillListFragment.java
+++ app/src/main/java/com/example/nemsy/BillListFragment.java
@@ -4,22 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 
 public class BillListFragment extends Fragment {
 
     BillAdapter adapter;
-
+    ArrayList ages = new ArrayList();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_bill_list, container, false);
 
         RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+        Spinner spinner = rootView.findViewById(R.id.spinner);
+
+        // spinner
+        for(int i=21; i>=10; i--) {
+            ages.add(i+"대");
+        }
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_spinner_item, ages);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
 
         // recyclerView
         LinearLayoutManager layoutManager = new LinearLayoutManager(rootView.getContext(), LinearLayoutManager.VERTICAL, false);
