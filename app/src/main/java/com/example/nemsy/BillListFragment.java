965accab19a355c542abb37f363bc0abd8bc89da
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-11-08 06:55:39 +0900

    feat: Volley로 발의법률안 웹 요청과 응답

--- app/src/main/java/com/example/nemsy/BillListFragment.java
+++ app/src/main/java/com/example/nemsy/BillListFragment.java
@@ -1,6 +1,7 @@
 package com.example.nemsy;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,17 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BillListFragment extends Fragment {
@@ -19,12 +30,18 @@ public class BillListFragment extends Fragment {
     BillAdapter adapter;
     ArrayList ages = new ArrayList();
 
+    static RequestQueue requestQueue;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_bill_list, container, false);
 
         RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
         Spinner spinner = rootView.findViewById(R.id.spinner);
+
+        if(requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getContext());
+        }
 
         // spinner
         for(int i=21; i>=10; i--) {
@@ -39,10 +56,11 @@ public class BillListFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new BillAdapter();
 
-        adapter.addItem(new Bill("민주유공자 예우에 관한 법률안", "21", "전재수의원 등 11인", "2022-11-03"));
-        adapter.addItem(new Bill("공직선거법 일부개정법률안", "21", "박성민의원 등 10인", "2022-11-03"));
+        adapter.addItem(new Bill("집합건물의 소유 및 관리에 관한 법률 일부개정법률안", "2020-05-22", "21", "http://likms.assembly.go.kr/bill/billDetail.do?billId=PRC_K2F0C0Y5B2D2C1Z5B2F6T4S2K9N8V8&ageFrom=20&ageTo=20", "김병관의원 등 13인"));
+        adapter.addItem(new Bill("집합건물의 소유 및 관리에 관한 법률 일부개정법률안", "2020-05-22", "21", "http://likms.assembly.go.kr/bill/billDetail.do?billId=PRC_K2F0C0Y5B2D2C1Z5B2F6T4S2K9N8V8&ageFrom=20&ageTo=20", "김병관의원 등 13인"));
 
         recyclerView.setAdapter(adapter);
+        makeRequest();
 
         adapter.setOnItemClickListener(new OnBillItemClickListener() {
             @Override
@@ -54,5 +72,34 @@ public class BillListFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    public void makeRequest() {
+        String url = "https://open.assembly.go.kr/portal/openapi/nzmimeepazxkubdpn?KEY=a9d0d8e5551d4a738e4f0e22fa5a0c4d&Type=json&pIndex=1&pSize=2&AGE=20";
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("응답", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("에러", error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+        };
+        request.setShouldCache(false);
+        requestQueue.add(request);
     }
 }
