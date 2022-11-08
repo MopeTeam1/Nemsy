5a064f2414a0a57b99e469f930914c7086f9c6b3
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-11-08 12:29:28 +0900

    feat: 발의법률안 상세 페이지 뒤로가기 버튼

--- app/src/main/java/com/example/nemsy/BillDetailActivity.java
+++ app/src/main/java/com/example/nemsy/BillDetailActivity.java
@@ -3,12 +3,24 @@ package com.example.nemsy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BillDetailActivity extends AppCompatActivity {
+    private ImageButton back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_detail);
+
+        back_button = (ImageButton) findViewById(R.id.back_button);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
