9842356646069dfb72d23a702d24038ac435a82c
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-11-08 12:16:08 +0900

    feat: 발의법률안 상세 fragment에서 activity로 변경

--- app/src/main/java/com/example/nemsy/BottomNavActivity.java
+++ app/src/main/java/com/example/nemsy/BottomNavActivity.java
@@ -1,28 +1,20 @@
 package com.example.nemsy;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BottomNavActivity extends AppCompatActivity {
-    BillListFragment billListFragment;
-    BillDetailFragment billDetailFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
-
-        billListFragment = (BillListFragment) getSupportFragmentManager().findFragmentById(R.id.bill_list);
-        billDetailFragment = new BillDetailFragment();
     }
 
-    public void onFragmentChanged(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation, billListFragment).commit();
-        }
-        else if (index == 1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation, billDetailFragment).commit();
-        }
+    public void onFragmentChanged() {
+        Intent intent = new Intent(getApplicationContext(), BillDetailActivity.class);
+        startActivity(intent);
     }
 }
