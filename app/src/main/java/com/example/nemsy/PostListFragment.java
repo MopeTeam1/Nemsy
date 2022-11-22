2b3c3d7825291b694caa1780a8503c2e2d747bbe
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-11-23 01:53:40 +0900

    [feat] postListFragment 완성

--- app/src/main/java/com/example/nemsy/PostListFragment.java
+++ app/src/main/java/com/example/nemsy/PostListFragment.java
@@ -1,10 +1,28 @@
 package com.example.nemsy;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PostListFragment extends Fragment {
 
     PostAdapter adapter;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_post_list, container, false);
+        RecyclerView recyclerView = rootView.findViewById(R.id.postRecyclerView);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(rootView.getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new PostAdapter();
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
 }
