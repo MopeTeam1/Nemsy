39a16d25883a5e93218db95f01c348c6274766f6
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-11-27 07:25:42 +0900

    [feat] CommunityPage : 글쓰기 버튼 누를시 WriteActivity 파일로 intent

--- app/src/main/java/com/example/nemsy/PostListFragment.java
+++ app/src/main/java/com/example/nemsy/PostListFragment.java
@@ -1,11 +1,14 @@
 package com.example.nemsy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,11 +18,13 @@ import com.example.nemsy.model.Post;
 public class PostListFragment extends Fragment {
 
     PostAdapter adapter;
+    AppCompatButton postButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_post_list, container, false);
         RecyclerView recyclerView = rootView.findViewById(R.id.postRecyclerView);
+        postButton = rootView.findViewById(R.id.postButton);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(rootView.getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
@@ -33,6 +38,14 @@ public class PostListFragment extends Fragment {
                 Post item = adapter.getItem(position);
                 BottomNavActivity activity = (BottomNavActivity) getActivity();
                 activity.putExtraPostIntent(item);
+            }
+        });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WriteActivity.class);
+                startActivity(intent);
             }
         });
 
